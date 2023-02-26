652a462f3066dcad19076ad12ffc6b90e7c1fe0e
Ocunidee <ocunidee@gmail.com>
2023-02-26 19:27:25 +0100

    Create template-driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, VERSION } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'atpw-template-driven-form'
+  versionName = `Angular ${VERSION.major}`
+  types = ['major', 'minor', 'patch']
+  selectedType = ''
 }
