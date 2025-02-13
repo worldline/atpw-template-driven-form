5fb13775eceee805c893dcc898b9e5f9adb7cc67
Ocunidee <ocunidee@gmail.com>
2025-02-13 21:25:54 +0100

    Transform simple template-driven form to reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component, VERSION } from '@angular/core'
-import { FormsModule, NgForm } from '@angular/forms'
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  versionName = `Angular ${VERSION.major}`
   types = ['major', 'minor', 'patch']
-  selectedType = ''
+  versionNameControl = new FormControl(`Angular ${VERSION.major}`, Validators.required)
+  typeControl = new FormControl('')
 
-  submitForm(form: NgForm) {
-    console.log('versionName:', this.versionName)
-    console.log('value of versionName field:', form.controls['versionName'].value)
-    console.log('selectedType:', this.selectedType)
-    console.log('form valid ?', form.valid)
+  submitForm() {
+    console.log('value of versionName control:', this.versionNameControl.value)
+    console.log('value of type control:', this.typeControl.value)
+    console.log('is form valid ?', this.versionNameControl.valid && this.typeControl.valid)
   }
 }
