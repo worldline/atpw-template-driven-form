f6fa0c0040195d71bbebd779b86625199883d8e1
Ocunidee <ocunidee@gmail.com>
2025-07-15 15:33:56 +0200

    Demonstrate the usage of NgForm and submit event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal, VERSION } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,11 @@ export class AppComponent {
   protected readonly versionName = signal(`Angular ${VERSION.major}`)
   protected readonly types = ['major', 'minor', 'patch']
   protected readonly selectedType = signal('')
+
+  submitForm(form: NgForm) {
+    console.log('versionName:', this.versionName())
+    console.log('value of versionName field:', form.controls['versionName'].value)
+    console.log('selectedType:', this.selectedType())
+    console.log('form valid ?', form.valid)
+  }
 }
